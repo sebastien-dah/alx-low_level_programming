2ad9b3277c2ec7ad6ad427b3b24bf0353d0c9d2b
sebastien-dah <133590565+sebastien-dah@users.noreply.github.com>
2023-07-17 18:44:27 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	int num;
 	
-	putchar('0');
-
-	for (num = 1; num < 10; num++)
+	for (num = 0; num < 10; num++)
 	{
-		putchar(',');
-		putchar(' ');
 		putchar(num + '0');
+		if(num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	}
 	return (0);
 }
